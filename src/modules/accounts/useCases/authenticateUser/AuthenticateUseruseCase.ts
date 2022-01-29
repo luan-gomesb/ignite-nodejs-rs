7adc8746f8bf2bb735e43a792eccb7e7acf8b25c
luan-gomesb <luan.gomesb@live.com>
2022-01-29 17:58:11 -0300

    authenticateUserUseCase criado

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUseruseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUseruseCase.ts
@@ -1,13 +1,40 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { User } from "../../entities/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
   email: string;
   password: string;
 }
-export default class AuthenticateUserUseCase {
+
+interface IResponse {
+  token: string,
+  user: { name: string, email: string };
+}
+class AuthenticateUserUseCase {
   constructor(private repository: IUsersRepository) { }
 
-  async execute({ email, password }: IRequest): Promise<void> {
-
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    //verifica se temos usuario
+    const user = await this.repository.findByEmail(email);
+    if (!user) {
+      throw new Error("Email or Password Incorrect")
+    }
+    //verifica se senha esta correta
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("Email or Password Incorrect")
+    }
+    const jwtoken = sign({}, "14b49a0684f6447465087515882f77d8", {
+      subject: user.id,
+      expiresIn: "1d"
+    })
+    const { name: userName, email: userEmail } = user;
+    return {
+      user: { name: userName, email: userEmail },
+      token: jwtoken
+    }
   }
 };
+export { AuthenticateUserUseCase }
