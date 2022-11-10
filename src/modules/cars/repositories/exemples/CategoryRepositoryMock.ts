d8b4871918643a03d53686234afe8856f7406654
Luan Gomes <luan.gomesn@live.com>
2022-11-10 15:02:37 -0300

    Correct CategoryRepositoryMock

--- src/modules/cars/repositories/exemples/CategoryRepositoryMock.ts
+++ src/modules/cars/repositories/exemples/CategoryRepositoryMock.ts
@@ -5,37 +5,33 @@ import { ICategoryRepository, ICreateCategoryDTO } from "../ICategoryRepository"
 class CategoryRepositoryMock implements ICategoryRepository {
 
   // GUARDARA A INSTANCIA DA CLASSE
-  private static INSTANCE: CategoryRepository;
+  private static INSTANCE: ICategoryRepository;
 
   private categories: Category[];
   //construtur da classe privado para somente a propria classe poder fazer uma instancia
   private constructor() {
     this.categories = [];
   }
-  getAll(): Category[] {
+  async getAll(): Promise<Category[]> {
     return this.categories;
   }
-  create({ name, description }: ICreateCategoryDTO): void {
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
     const category = new Category(name, description);
     this.categories.push(category);
   }
 
-  findByName(name: string): Category {
+  async findByName(name: string): Promise<Category> {
     return this.categories.find((category) => category.name === name);
   }
 
-  static getInstance(): CategoryRepository {
+  static getInstance(): ICategoryRepository {
     //verifica se já temos instancia
     if (!this.INSTANCE) {
-      this.INSTANCE = new CategoryRepository();
+      this.INSTANCE = new CategoryRepositoryMock();
     }
     //retorna a instancia estatica
     return this.INSTANCE;
   }
-
-
-
-
 }
 
-export { CategoryRepository }
+export { CategoryRepositoryMock }
